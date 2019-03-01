8be3d023882708a8ea65a1d74c870aa89f49495c
luo <2453682506@qq.com>
2019-03-01 12:29:44 +0800

    价格导入实体类

--- guns-parent/guns-admin/src/main/java/com/stylefeng/guns/modular/system/utils/IntegralRecordTypeExcel.java
+++ guns-parent/guns-admin/src/main/java/com/stylefeng/guns/modular/system/utils/IntegralRecordTypeExcel.java
@@ -7,38 +7,26 @@ import cn.afterturn.easypoi.excel.annotation.ExcelTarget;
 public class IntegralRecordTypeExcel {
 
 
-    @Excel(name = "门店名称",orderNum = "1")
-    private String WarehouseName;
-    @Excel(name = "门店编码",orderNum = "2")
-    private String WarehouseCode;
-    @Excel(name = "商品编码",orderNum = "3")
+//    @Excel(name = "门店名称",orderNum = "1")
+//    private String WarehouseName;
+//    @Excel(name = "门店编码",orderNum = "2")
+//    private String WarehouseCode;
+    @Excel(name = "商品编码",orderNum = "1")
     private String InventoryCode;
-    @Excel(name = "商品名称",orderNum = "4")
-    private String InventoryName;
-    @Excel(name = "规格",orderNum = "5")
-    private String Specification;
-    @Excel(name = "可用数量",orderNum = "6")
-    private Double AvailableQuantity;
-    @Excel(name = "计量单位",orderNum = "7")
-    private String UnitName;
-    @Excel(name = "商品类型",orderNum = "8")
-    private Integer producttype;
-
-    public String getWarehouseName() {
-        return WarehouseName;
-    }
-
-    public void setWarehouseName(String warehouseName) {
-        WarehouseName = warehouseName;
-    }
-
-    public String getWarehouseCode() {
-        return WarehouseCode;
-    }
-
-    public void setWarehouseCode(String warehouseCode) {
-        WarehouseCode = warehouseCode;
-    }
+    @Excel(name = "亲民价",orderNum = "2")
+    private Double productpice;
+    @Excel(name = "零售价",orderNum = "3")
+    private Double retailPrice;
+//    @Excel(name = "商品名称",orderNum = "4")
+//    private String InventoryName;
+//    @Excel(name = "规格",orderNum = "5")
+//    private String Specification;
+//    @Excel(name = "可用数量",orderNum = "6")
+//    private Double AvailableQuantity;
+//    @Excel(name = "计量单位",orderNum = "7")
+//    private String UnitName;
+//    @Excel(name = "商品类型",orderNum = "8")
+//    private Integer producttype;
 
     public String getInventoryCode() {
         return InventoryCode;
@@ -48,43 +36,19 @@ public class IntegralRecordTypeExcel {
         InventoryCode = inventoryCode;
     }
 
-    public String getInventoryName() {
-        return InventoryName;
+    public Double getProductpice() {
+        return productpice;
     }
 
-    public void setInventoryName(String inventoryName) {
-        InventoryName = inventoryName;
+    public void setProductpice(Double productpice) {
+        this.productpice = productpice;
     }
 
-    public String getSpecification() {
-        return Specification;
+    public Double getRetailPrice() {
+        return retailPrice;
     }
 
-    public void setSpecification(String specification) {
-        Specification = specification;
-    }
-
-    public Double getAvailableQuantity() {
-        return AvailableQuantity;
-    }
-
-    public void setAvailableQuantity(Double availableQuantity) {
-        AvailableQuantity = availableQuantity;
-    }
-
-    public String getUnitName() {
-        return UnitName;
-    }
-
-    public void setUnitName(String unitName) {
-        UnitName = unitName;
-    }
-
-    public Integer getProducttype() {
-        return producttype;
-    }
-
-    public void setProducttype(Integer producttype) {
-        this.producttype = producttype;
+    public void setRetailPrice(Double retailPrice) {
+        this.retailPrice = retailPrice;
     }
 }
