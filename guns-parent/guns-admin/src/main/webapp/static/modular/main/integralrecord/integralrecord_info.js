382ecef201329a273a431afd1e7684eb4e25e960
cyq <1418806370@qq.com>
2019-03-22 12:50:05 +0800

    验证码

--- guns-parent/guns-admin/src/main/webapp/static/modular/main/integralrecord/integralrecord_info.js
+++ guns-parent/guns-admin/src/main/webapp/static/modular/main/integralrecord/integralrecord_info.js
@@ -163,49 +163,49 @@ $(function() {
     Feng.initValidator("integralrecordInfoTable", IntegralrecordInfoDlg.validateFields);
 
     //绑定商品搜索
-    $('.selectpicker').selectpicker({
-        'selectedText': 'cat'
-    });
-    $(".selectpicker" ).selectpicker('refresh');
-    $(".bs-searchbox input").keyup(function(event){
-        // if(event.keyCode == "13") {//判断如果按下的是回车键则执行下面的代码
-            var search=($(".bs-searchbox input").val())
-            //进行搜索 integralrecordtype/list
-            var ajax = new $ax(Feng.ctxPath + "/integralrecordtype/list", function (data) {
-                console.log(data)
-                $("#productname").empty();
-                if(data.rows.length>0){
-                    for(var i=0;i<data.rows.length;i++){
-                        $("#productname").append("<option value='"+data.rows[i].id+"'>【"+data.rows[i].productname+"】 库存("+data.rows[i].productnum+")</option>");
-                    }
-                }
-                $(".selectpicker" ).selectpicker('refresh');
-            }, function (data) {
-            });
-            ajax.set("condition", search);
-            ajax.set("producttype", $("#typeId").val());
-            ajax.set("limit", 9999);
-            ajax.start();
-        // }
-
-    });;
-    $("#typeId").change(function () {
-        changeselect();
-
-
-    })
-    $("#productname").change(function () {
-      //查询商品
-        findbyid();
-
-    })
-    changeselect();
-    findbyid();
-    $("#play").keyup(function(event){
-        console.log($("#play").val())
-       changejifen();
-
-    })
+    // $('.selectpicker').selectpicker({
+    //     'selectedText': 'cat'
+    // });
+    // $(".selectpicker" ).selectpicker('refresh');
+    // $(".bs-searchbox input").keyup(function(event){
+    //     // if(event.keyCode == "13") {//判断如果按下的是回车键则执行下面的代码
+    //         var search=($(".bs-searchbox input").val())
+    //         //进行搜索 integralrecordtype/list
+    //         var ajax = new $ax(Feng.ctxPath + "/integralrecordtype/list", function (data) {
+    //             console.log(data)
+    //             $("#productname").empty();
+    //             if(data.rows.length>0){
+    //                 for(var i=0;i<data.rows.length;i++){
+    //                     $("#productname").append("<option value='"+data.rows[i].id+"'>【"+data.rows[i].productname+"】 库存("+data.rows[i].productnum+")</option>");
+    //                 }
+    //             }
+    //             $(".selectpicker" ).selectpicker('refresh');
+    //         }, function (data) {
+    //         });
+    //         ajax.set("condition", search);
+    //         ajax.set("producttype", $("#typeId").val());
+    //         ajax.set("limit", 9999);
+    //         ajax.start();
+    //     // }
+    //
+    // });;
+    // $("#typeId").change(function () {
+    //     changeselect();
+    //
+    //
+    // })
+    // $("#productname").change(function () {
+    //   //查询商品
+    //     findbyid();
+    //
+    // })
+    // changeselect();
+    // findbyid();
+    // $("#play").keyup(function(event){
+    //     console.log($("#play").val())
+    //    changejifen();
+    //
+    // })
 });
 function changeselect() {
     var ajax = new $ax(Feng.ctxPath + "/integralrecordtype/list", function (data) {
